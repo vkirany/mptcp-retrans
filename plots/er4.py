03004228d2a6c15d15957955fabd91ef05d50ab4
Kiran <kiran.yedugundla@kau.se>
2016-10-18 14:51:16 +0200

    Update

--- plots/er4.py
+++ plots/er4.py
@@ -5,15 +5,18 @@ import matplotlib.pyplot as plt
 
 n_groups = 5 
 
-s2020 = (183,481)
+b1 = (183,185,214,185,236)
+b2 = (481,542,532,552,887)
 
-s2030 = (185,542)
+#s2020 = (183,481)
 
-s3020 = (214,532)
+#s2030 = (185,542)
 
-s20120 = (185,552)
+#s3020 = (214,532)
 
-s12020 = (236,887)
+#s20120 = (185,552)
+
+#s12020 = (236,887)
 
 
 fig, ax = plt.subplots()
@@ -23,30 +26,15 @@ bar_width = 0.18
 
 opacity = 0.4
 
-rects1 = plt.bar(index, s2020 , bar_width,
+rects1 = plt.bar(index, b1 , bar_width,
                  alpha=opacity,
                  color='b',
-                 label='20-20')
+                 label='Burst1')
 
-rects2 = plt.bar(index + bar_width, s2030, bar_width,
+rects2 = plt.bar(index + bar_width, b2, bar_width,
                  alpha=opacity,
                  color='r',
-                 label='20-30')
-
-rects3 = plt.bar(index + 2*bar_width, s3020, bar_width,
-                 alpha=opacity,
-                 color='g',
-                 label='30-20')
-
-rects4 = plt.bar(index + 3*bar_width, s20120, bar_width,
-                 alpha=opacity,
-                 color='m',
-                 label='20-120')
-
-rects5 = plt.bar(index + 4*bar_width, s12020, bar_width,
-		alpha=opacity,
-		color='c',
-		label='120-20')
+                 label='Burst2')
 
 
 plt.xlabel('Scenarios')
@@ -55,7 +43,7 @@ plt.ylabel('AvgDelay (ms)')
 plt.xticks(0.185 + index + bar_width, ('20-20', '20-30', '30-20', '20-120', '120-20'))
 #plt.legend()
 plt.tight_layout()
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=4)
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=5)
 plt.savefig('avgDelay.pdf')
 
 #plt.show()
